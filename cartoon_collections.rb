2808fae67d711e1da0f7f29a57c47234fb41e4ee
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-08 00:03:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
-    index += 1 
-    puts "#{index}, #{name}"
+    index += 1
+    puts "#{index}. #{name}"
   end
 end
 
@@ -18,9 +18,11 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  i = 0 
-  cheese_types = ["cheddar", "gouda","camembert"]
+  # the array below is here to help
+  i = 0
+  cheese_types = ["cheddar", "gouda", "camembert"]
   while i < cheese_types.length
     return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1
   end
 end
